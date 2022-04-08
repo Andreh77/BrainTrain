8574706e371c345f3a7ae5c655aacce2ccfb174c
BlackCook <andreh1967@windowslive.com>
2022-04-09 01:46:34 +1200

    Andre-MainMenu
    -Cant quit the game when playing on build
    -Added menu music which can be paused and played with button.

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public Color normal, header, background;
+    public ButtonManager selectedButton;
+
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
