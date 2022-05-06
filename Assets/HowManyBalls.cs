1256fc77032753e3ff3e92ba980afb16f7ad5132
Andre <andreh1967@windowslive.com>
2022-05-06 13:21:49 +1200

    Andre-MainMenuUI&&ClickCirclesGame

--- Assets/HowManyBalls.cs
+++ Assets/HowManyBalls.cs
@@ -4,22 +4,30 @@ using UnityEngine;
 using TMPro;
 public class HowManyBalls : GameMode
 {
-    public GameObject InstructionsScreen, guessScreen;
+    public GameObject startUI;
 
     public double startTime;
     public double finishTime;
     public double totalTime;
 
-    public bool timerStarted = false;
-    public int numberOfBalls = 1;
-
+    public bool timerStart = false;
+    public int numberOfBalls = 10;
+ 
     public TMP_InputField inputField;
-
+    public TMP_Text scoresUI;
+    public List<double> scores = new List<double>();
     public GameObject ball;
-    
+
+    public int level = 1;
     private void Start()
     {
-        numberOfBalls = 1;
+        startUI.SetActive(true);
+        numberOfBalls = level * 2;
+        SpawnBalls();
+    }
+
+    public void SpawnBalls()
+    {
         for (int i = 0; i < numberOfBalls; i++)
         {
             GameObject GO = Instantiate(ball);
@@ -29,42 +37,50 @@ public class HowManyBalls : GameMode
 
     private void Update()
     {
-        if(timerStarted == false && Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Mouse0))
         {
-            InstructionsScreen.SetActive(false);
-            startTime = Time.timeSinceLevelLoadAsDouble;
-            timerStarted = true;
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                Debug.Log("Target Position: " + hit.collider.gameObject.transform.position);
+                Destroy(hit.collider.gameObject);
+                numberOfBalls--;
+            }
+            else
+            {
+                //Maybe add a penalty if they miss click? // LOSE ALL LEVEL PROGRESS
+            }
         }
-        else if(Input.GetKeyDown(KeyCode.Space))
+
+        if(timerStart == false && Input.GetKeyDown(KeyCode.Space))
+        {
+            startUI.SetActive(false);
+            startTime = Time.timeSinceLevelLoadAsDouble;
+            timerStart = true;
+        }
+        
+        if(numberOfBalls == 0 && timerStart == true)
         {
             finishTime = Time.timeSinceLevelLoadAsDouble;
             totalTime = finishTime - startTime;
-            
-            guessScreen.SetActive(true);
-            inputField.ActivateInputField();
+            timerStart = false;
+            scores.Add(totalTime);
+            startUI.SetActive(true);
+ 
+            //inputField.ActivateInputField();
+ 
+            NextLevel();
         }
     }
 
-    public void CheckAnwser()
+    public void NextLevel()
     {
-        int guess = int.Parse(inputField.text);
-        if (guess == numberOfBalls)
-        {
-            Correct();
-        }
-        else
-        {
-            Incorrect();
-        }
-    }
-
-    public void Correct()
-    {
-        Debug.Log("It took you: " + totalTime + " to anwser correctly.");
-    }
-
-    public void Incorrect()
-    {
-        Debug.Log("Wrong anwser");
+        scoresUI.text += "Level " + (level) + " Score: " + scores[scores.Count - 1].ToString("F4") + "seconds \n";
+        level++;
+        numberOfBalls = level * 2;
+        finishTime = 0;
+        startTime = 0;
+        SpawnBalls();
     }
 }
