b2832e7c8e24463a8a61cf17ba54c57afcd249a0
ShayneMuir <102783806+ShayneMuir@users.noreply.github.com>
2022-05-07 17:23:10 +1200

    Shayne - Created Player Object

---
+++ Assets/Player.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Player
+{
+    public string name;
+    public int overallScore;
+    [SerializeField] public List<GameScore> gameScores = new List<GameScore>();
+
+    public Player(string _name)
+    {
+        name = _name;
+    }
+
+    public void addGameScore(GameScore _gameScore)
+    {
+        gameScores.Add(_gameScore);
+    }
+}
+
+[System.Serializable]
+public class GameScore
+{
+    public string gameName;
+    public double score;
+}
